b8868a56ed170b6866df1a4cec1b9c2557e5f91f
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-07 00:03:52 +0530

    Create 03-dataTypes.js

---
+++ JavaScript/Lec18-JS BASIC/03-dataTypes.js
@@ -0,0 +1,16 @@
+/* There are two types of DataTypes.
+     1. Primitive Types.                    2. Reference Types
+        a. String                             a. Objects
+        b. Number                             b. Arrays
+        c. Boolean                            c. Functions
+        d. Undefined
+        e. Null
+*/
+
+//    Dynamic Typing
+
+ let  lastName = 'Yadav';
+ console.log(lastName);
+
+ lastName = 56;
+ console.log(lastName);
