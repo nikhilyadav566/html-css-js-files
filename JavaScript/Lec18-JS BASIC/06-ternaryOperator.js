34c68ed9cbd0bce864c35de383b1d110177867e3
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-08 01:22:56 +0530

    Create 06-ternaryOperator.js

---
+++ JavaScript/Lec18-JS BASIC/06-ternaryOperator.js
@@ -0,0 +1,12 @@
+//   Ternary Operator.
+
+//    condition ? value1 : value2;
+// if condition will be true then print value1 otherwise value2.
+
+let age=25;
+
+let status = (age>=18) ? 'I can vote':'Cannot vote';
+
+console.log(status);
+// Output : I can vote
+// if false then return Cannot vote
