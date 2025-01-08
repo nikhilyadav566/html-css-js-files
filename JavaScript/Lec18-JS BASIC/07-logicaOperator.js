59422b7ae52aa8ab7cc7a2443a40ae51716d9c47
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-09 03:31:19 +0530

    Update 07-logicaOperator.js

--- JavaScript/Lec18-JS BASIC/07-logicaOperator.js
+++ JavaScript/Lec18-JS BASIC/07-logicaOperator.js
@@ -7,11 +7,11 @@
 //  1. AND (&&) :- All conditions have to be true
 
 let a = 15;
-let b = 16;
+let b = 16;  
 let c = 17;
 
-console.log(a<b && c>b && a<c);
-console.log(b>a && a>c && b<c);
+console.log(a<b && c>b && a<c);          //  true
+console.log(b>a && a>c && b<c);          // false
 
 //  2. OR (||)  :- Any single condition have to be true.
 
@@ -19,8 +19,8 @@ let d = 3;
 let e = 4;
 let f = 5;
 
-console.log(d<e || e<f || f>d);
-console.log(d>e || f<d || e>f);
+console.log(d<e || e<f || f>d);              // true
+console.log(d>e || f<d || e>f);              // false
 
 // 3. NOT :- it gives the opposite ans if result will be true then after applying this operator 
 //  result become false similary false result become true. 
@@ -29,5 +29,5 @@ let num1 = 7;
 let num2 = 8;
 let num3 = 9;
 
-console.log(!(num1>num2 && num2>num3 && num3<num1));
-console.log(!(num1<num2 && num2<num3 && num3>num1));;
+console.log(!(num1>num2 && num2>num3 && num3<num1));                // true
+console.log(!(num1<num2 && num2<num3 && num3>num1));                //  false
