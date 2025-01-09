eec9bff178e1f7a8c71c7d6f70e65810c494ce51
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-10 03:50:11 +0530

    Create 09-bitwiseOperator.js

---
+++ JavaScript/Lec18-JS BASIC/09-bitwiseOperator.js
@@ -0,0 +1,13 @@
+//  Basically  There are two types of Bitwise Operator.
+
+//   1. Bitwise AND (&)                           2. Bitwise OR (|)
+
+//  Bits:- 
+//         0:- zero means false.
+//         1:- one means true.
+
+
+let a = 2;
+let b = 3 ;
+console.log(a & b);      // Output : -> 2
+console.log(a | b);      //  3
