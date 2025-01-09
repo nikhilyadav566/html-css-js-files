f9cda65a92abaf5e2d648b5da5c48651118f651e
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-10 03:51:14 +0530

    Create 10-controlStatements.js

---
+++ JavaScript/Lec18-JS BASIC/10-controlStatements.js
@@ -0,0 +1,35 @@
+//  If-else And Swith comes under Control Statements.
+
+// 1. If-elseif-else
+// syntax:-
+//          if(condition){
+//                   statements   
+//             }
+//          else if(condition){ 
+//                  statements
+//             }
+//          else{   statements
+//                      }
+
+let marks=65;
+
+if(marks>=90){
+    console.log('A Grade');
+}
+else if(marks>=80){
+    console.log('B Grade');
+}
+else if(marks>=70){
+    console.log('C Grade');
+}
+else if(marks>=60){
+    console.log('D Grade');
+}
+else if(marks>=50){
+    console.log('E Grade');
+}
+else{
+    console.log('fail')
+}
+
+//  Output :->  D Grade
