40d0880e15a6d46881d515cefc5a697dfbc3ee78
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-11 02:07:05 +0530

    Create 12-loops.js

---
+++ JavaScript/Lec18-JS BASIC/12-loops.js
@@ -0,0 +1,37 @@
+// Loops:-  Loops are used for repeatation of same task .
+
+// Basically there are three types of loops.
+
+// 1. For Loop         2. While Loop            3. Do-While 
+
+// 1. for loop:-
+
+// syntax:-
+//         for(initialization ; condition ; updation){
+//             statements;
+//            }
+
+for(let i = 0; i<=9; i=i+1){     
+    console.log(i);           //  Output :-> 0 to 9 counting no
+}
+
+for(let i=1; i<=5; i+=1){    
+    console.log('most welcome');    //  Output  :->  5times print most welcome
+}
+
+for(let i =2; i<=20; i=i+2){
+    console.log(i);               
+}
+
+//  Output :->
+
+//  2
+//  4
+//  6
+//  8
+//  10
+//  12
+//  14
+//  16
+//  18
+//  20
