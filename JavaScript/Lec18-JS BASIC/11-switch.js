639ffc4766428fec1ed7a4be30ab861be0c1da17
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-11 02:06:37 +0530

    Create 11-switch.js

---
+++ JavaScript/Lec18-JS BASIC/11-switch.js
@@ -0,0 +1,25 @@
+ //      switch case
+
+ //  syntax:-
+ //          switch(expre ssion){
+//              case_:console.log();
+//              break;
+//              case: console.log();
+//              break;
+//              default:console.log();
+//              }
+
+let num=2;
+
+switch(num) {
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
+
+
+//  output : -> B
