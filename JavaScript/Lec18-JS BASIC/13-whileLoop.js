ac4632fce02089422325157d429e388db8054995
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-12 02:23:04 +0530

    Create 13-whileLoop.js

---
+++ JavaScript/Lec18-JS BASIC/13-whileLoop.js
@@ -0,0 +1,30 @@
+// syntax:-
+//          initialization;
+//          while(condition){
+//              statements;
+//           updation;
+//          }  
+
+let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+//  Output:->
+//  1 
+//  2 
+//  3 
+//  4 
+//  5
+
+let x=0;
+while(x<=7){
+    console.log('Hii');
+    x+=2;
+}
+
+//  Output: ->
+//  Hii 
+//  Hii 
+//  Hii
