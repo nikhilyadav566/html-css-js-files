31824ec26b92395fb000546241acac0b772af419
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-12 02:23:51 +0530

    Create 14-doWhileLoop.js

---
+++ JavaScript/Lec18-JS BASIC/14-doWhileLoop.js
@@ -0,0 +1,23 @@
+ // syntax:-
+ //            initialization;
+ //            do{   
+//                  statements;
+//                  updation;
+//                }while(condition);
+
+//   It execute atleast 1 time.
+
+let i=0;
+
+do{
+    console.log(i);
+    i++;
+}while(i<=8);
+
+
+
+let y=1;
+do{
+    console.log('*');
+    y++;
+}while(y<=5);
