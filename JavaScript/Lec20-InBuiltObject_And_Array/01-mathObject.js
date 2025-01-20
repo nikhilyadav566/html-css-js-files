1ed2025215c13f3fedeb3483968e2bc20fb66933
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-20 21:02:12 +0530

    Create 01-mathObject.js

---
+++ JavaScript/Lec20-InBuiltObject_And_Array/01-mathObject.js
@@ -0,0 +1,15 @@
+
+
+console.log(Math.PI); // Ratio of circle'circumference to its diametr 
+console.log(Math.E); // Euler's number 
+console.log(Math.SQRT1_2);  // Square root of 1/2 
+console.log(Math.SQRT2);     // Square root of 2
+console.log(Math.abs(-98));   // return absolute value
+console.log(Math.max(1,5,6,7,));  // returns maximum no
+console.log(Math.min(1,5,6,7,9));  // returns mininum no.
+console.log(Math.round(5.6));    // returns the value of the input rounded to the nearest integer.
+console.log(Math.sign(-2.59));  // returns the sign of the input, indicating whether it is positive , negative , or zero.
+console.log(Math.sqrt(2));  // returns the positive square root of the input.
+console.log(Math.tan(30));  // returns the value of tan30.
+
+// There are so many methods of math object.
