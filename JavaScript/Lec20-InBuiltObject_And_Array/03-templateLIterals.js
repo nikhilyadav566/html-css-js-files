8e4832f31614346cbc856412a4a1cf9f1f89175d
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-21 07:29:29 +0530

    Create 03-templateLIterals.js

---
+++ JavaScript/Lec20-InBuiltObject_And_Array/03-templateLIterals.js
@@ -0,0 +1,24 @@
+//  03-template Literals
+//  (` `):-> through backtics user can print as it is.
+
+let message = 
+`This 
+is    
+my first 
+message`;
+
+console.log(message);
+
+//  2nd Type 
+
+let firstName = 'Nikhil';
+
+let msg = 
+`Hello ${firstName},
+
+Thanks for the Opportunity
+
+Regards,
+Nikhil`;
+
+console.log(msg); 
