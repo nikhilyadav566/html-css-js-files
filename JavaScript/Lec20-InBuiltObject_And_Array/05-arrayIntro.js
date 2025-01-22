f02421298cbf5d27f719cad2747cee6b36701c40
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-22 16:27:07 +0530

    Create 05-arrayIntro.js

---
+++ JavaScript/Lec20-InBuiltObject_And_Array/05-arrayIntro.js
@@ -0,0 +1,12 @@
+// Creation of array
+
+let num = [5,7,6,9];
+console.log(num);      //  Output : [ 5, 7, 6, 9 ]
+
+
+
+
+// Note:  here no. is access by index no.
+let arr = [9,7,8,4]
+console.log(arr[2]);  // output: 8 because its index no is 2.
+ 
