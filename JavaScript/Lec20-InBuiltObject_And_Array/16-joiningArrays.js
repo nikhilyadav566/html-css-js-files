e4f6f6d69bc0ac5ef2d18992a3495a6a9144348e
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-28 10:15:41 +0530

    Create 16-joiningArrays.js

---
+++ JavaScript/Lec20-InBuiltObject_And_Array/16-joiningArrays.js
@@ -0,0 +1,10 @@
+//   Joining Arrays
+//                  join():-> join method in JavaScript to convert an array into a string.
+
+let arr = [21,45,85,75,'Nikhil'];   // The array arr contains elements of different data types: [21, 45, 85, 75, 'Nikhil']
+
+
+let value = arr.join(',');
+console.log(value);    //  Output : 21,45,85,75,Nikhil
+
+//  join :-> The join(',') method joins all the elements of the array into a single string, using a comma , as the separator.
