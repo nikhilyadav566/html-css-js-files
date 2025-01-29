0f362b28558b4a7d5c84a522f60af843a6b626f9
Nikhil Yadav <nikhilyad42004@gmail.com>
2025-01-29 10:59:19 +0530

    Create 18-sortingArrays.js

---
+++ JavaScript/Lec20-InBuiltObject_And_Array/18-sortingArrays.js
@@ -0,0 +1,13 @@
+//  Sorting Arrays
+
+let numbers = [ 40, 30, 10, 20,50]
+
+let sorted = numbers.sort();
+console.log(sorted);      //  Output : [ 10, 20, 30, 40, 50 ]
+
+
+
+//  Reverse Arrays
+
+let num2 = numbers.reverse();
+console.log(num2);              // Output : [ 50, 40, 30, 20, 10 ]
