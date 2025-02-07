70cbd809bbde6ff0965c0017bf744830a51d9f30
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-07 23:16:52 +0530

    simple DOM

---
+++ JavaScript/Lec22-DOM/02-DOM.js
@@ -0,0 +1,15 @@
+//    DOM stands for Document Object Model.
+//    Js convert all the html code in js object is called DOM.
+//    console.log(document.body)
+//     it means prints body.
+
+
+console.log(document);
+console.log(document.body);
+
+//  it's tree like structure.
+
+//  DOM is created through this process.
+//  character -> tags -> token -> nodes -> DOM
+
+//  tags are converted by tokenizer.
