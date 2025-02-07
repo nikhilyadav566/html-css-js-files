82d1574cb2d51dcf418345746f6264f3fbd4f0f3
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-07 23:12:48 +0530

    Add basic file of DOM

---
+++ JavaScript/Lec22-DOM/01-windowObject.js
@@ -0,0 +1,12 @@
+//   Window Object
+
+//   Window is a global Object.
+//   Represented by Browser window.
+//   Wondow have - DOM , BOM , JS CODE
+
+
+
+// Window Object
+window.console.log('Window Object');
+// alert('window.console.log')
+// window.alert('Alert');  
