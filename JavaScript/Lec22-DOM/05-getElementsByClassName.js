042be147b1cd02180ffc2934a3521782937ef664
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-09 00:22:46 +0530

    Add new file

---
+++ JavaScript/Lec22-DOM/05-getElementsByClassName.js
@@ -0,0 +1,8 @@
+//   The getElementsByClassName method of document interfae returns an array-like object of all child elements which have all of the given class name(s).
+
+//   It returns multiple object.
+
+//  document.getElementByClassName(' ')
+
+let element = document.getElementsByClassName('web');
+   console.log(element);
