5db8f55ca21546d98efcc7477e26e55e437f89cd
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-10 00:38:20 +0530

    Add new file

---
+++ JavaScript/Lec22-DOM/07-$0.js
@@ -0,0 +1,8 @@
+//  It returns the most recently selected element or JavaScript Object.
+
+//     $0   (dollar-zero)
+
+//  Returns the most recently selector element or javaScript Object.
+
+let item = $0;
+console.log(item);
---
+++ JavaScript/Lec22-DOM/08-querySelector.js
@@ -0,0 +1,16 @@
+//  querySelector() 
+
+//   we can give className by .(dot) and idName (#),  tagName.
+//   It return single object.
+
+//   #1  - class
+let classItem = document.querySelector('.nextSlide');
+console.log(classItem);
+
+//    #2   - id 
+let idItem = document.querySelector('#header');
+  console.log(idItem);
+
+//   #3   - tagName
+let tagItem  = document.querySelector('header');
+  console.log(tagItem);
