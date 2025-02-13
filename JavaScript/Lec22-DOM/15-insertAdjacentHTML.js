87a88533e8a3b701ab2192a583dd7db23ed28870
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-13 12:47:51 +0530

    Add new file

---
+++ JavaScript/Lec22-DOM/15-insertAdjacentHTML.js
@@ -0,0 +1,31 @@
+  //  insertAdjacentHTML():-
+  //                it has to be called 2 arguments
+  //   1.             location / Position  -> where
+  //   2.            HTML text / content to be inserted -> what
+
+
+  //  Now, There are four location where we can insert.
+  //   1.  beforebegin      ->   It insert HTML text as previous sibling.       
+  //   2.  afterbegin       ->   It insert HTML text as child.
+  //   3.  before end       ->   
+  //   4.  after end
+
+
+  let parent = document.querySelector('#list');
+// console.log(parent);
+
+// Create Child 
+let child1 = document.createElement('li');
+let content1 = document.createTextNode("Before End");
+child1.appendChild(content1);
+
+// As a Element we can send our element 
+parent.insertAdjacentElement('beforeend',child1);
+
+
+let child2 = document.createElement('li');
+let content2 = document.createTextNode("Before Begin");
+child2.appendChild(content2);
+
+// As a HTML SEnd - using stirng 
+parent.insertAdjacentHTML('beforebegin','<li>BEfore Begin</li>')
