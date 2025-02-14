f4efbc0caa6ed0b273606e390c8be91d8b02a78a
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-14 23:08:07 +0530

    Add new file

---
+++ JavaScript/Lec22-DOM/16-removeElement.js
@@ -0,0 +1,12 @@
+// removeChild()
+//   Opposite of apepndChild()
+//   parent element
+//   the child element to remove
+
+//  ex:- parent.removeChild(childElement)    // both parent and childElement should be known.
+
+
+let element = document.querySelector('#thnku');
+// console.log(element);
+
+element.remove()
