c48d6c82fc98cde05867c06c3cd60f56190dba00
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-17 02:44:25 +0530

    Add new file

---
+++ JavaScript/Lec23-BOM/01-monitorEvents.js
@@ -0,0 +1,7 @@
+//  BOM :-> Browser Object Model
+
+// RUN CODE IN CONSOLE 
+// SEE all events 
+console.log(monitorEvents(document));
+  
+//  event: It refers to an action that can be detected and responded to with in a web application.
