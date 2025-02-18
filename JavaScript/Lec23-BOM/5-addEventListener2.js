86bef3b57f03845ae864b5c621bcc752b2d0880e
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-19 03:26:43 +0530

    Add new file

---
+++ JavaScript/Lec23-BOM/5-addEventListener2.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener('click',function(){
+    document.body.style.backgroundColor = "lightblue";
+    console.log('I have clicked on the document');
+});
+
+//  2nd Method
+
+// function changeColor(){
+//     document.body.style.backgroundColor = "green";
+// }
+// document.addEventListener('click', changeColor);
+
+
+document.addEventListener('click',function(){
+    document.body.style.backgroundColor = "lightblue";
+    console.log('I have clicked on the document');
+});
+
+//  2nd Method
+
+// function changeColor(){
+//     document.body.style.backgroundColor = "green";
+// }
+// document.addEventListener('click', changeColor);
