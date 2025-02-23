84a8efbc133d53dcf25ff8edb551c426cc8acc10
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-23 13:03:49 +0530

    update output

--- JavaScript/Lec22-DOM/11-createElement.js
+++ JavaScript/Lec22-DOM/11-createElement.js
@@ -1,5 +1,8 @@
-newElement = document.createElement('h1') //create h1 
+newElement = document.createElement('h1') //create <h1> element
 // add content in h1 
-let content = document.createTextNode("This is Heading");
-newElement.appendChild(content);
-console.log(newElement)  
+let content = document.createTextNode("This is Heading");  // create text which is used inside element
+newElement.appendChild(content);   // Insert the text node inside the <h1> element
+console.log(newElement)       //  now prints it
+
+//  Out will be 
+//  <h1> This is Heading </h1>
--- JavaScript/Lec22-DOM/13-appendChild.js
+++ JavaScript/Lec22-DOM/13-appendChild.js
@@ -1,7 +1,16 @@
 let parent = document.querySelector('#list');
 
-let newElement = document.createElement('h1') //create h1 
-// add content in h1 
+let newElement = document.createElement('h2') //create h2 
+// add content in h2 
 let content = document.createTextNode("This is Heading");
 newElement.appendChild(content);     // to add we should use .appendChild()
 parent.appendChild(newElement);
+
+// suppose it is given in html file
+// <div id ='list'></div>
+
+//  The output of that above js code after execution will be 
+
+//  <div id = 'list'>
+//     <h2>This is Heading</h2>
+//   </div>
