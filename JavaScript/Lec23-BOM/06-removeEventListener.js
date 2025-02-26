c527cd220303706560619f692aaa3c2937229983
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-02-27 01:02:03 +0530

    Initial commit

--- JavaScript/Lec23-BOM/06-removeEventListener.js
+++ JavaScript/Lec23-BOM/06-removeEventListener.js
@@ -4,7 +4,15 @@
 //             the comparison returns true; otherwise, it returns false.
 
 // loose equality (==) :-> it allows Type Coercion.
+     let a = 5;
+     let d = '5';
+     console.log(a == d); // true
+
+
 // strict equality (===) :-> where it doesn't allow Type Coercion.(it only prevent from it  ).
+     let e = 9;
+     let f = '9';
+     console.log(e === f); // false
 
 
 // function print(){
@@ -30,5 +38,5 @@ document.removeEventListener('click',eventFunction);
  //   There are three conditions are needed to remove events.
  // 1. Same target :-> (document,p,h1, div, article,video etc)
  // 2. Same type   :-> (click,dbclick,scroll etc)
- // 3. Same function :-> (define:-> waht to do when event happen)
- //     here eventFunction is a function.
+ // 3. Same function :-> (define:-> what to do when event happen)
+ //    and here eventFunction is a function.
