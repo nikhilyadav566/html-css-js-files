57c03b19b5170f04a58b399d8affa0f6d85e857b
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-03-02 01:14:28 +0530

    Initial commit

---
+++ JavaScript/Lec24-Event_Loop/08-setTimeOut1.js
@@ -0,0 +1,22 @@
+//  setTimeOut
+//           It is a method which is used to execute a function after a specified delay (in milliseconds).
+
+//  Prameters: 
+// 1. function
+// 2. time in ms (delay)
+
+// Syntax :
+//   setTimeout(function, delay, param1, param2, ...);
+//   function → The callback function to execute after the delay.
+//   delay → Time in milliseconds (1 sec = 1000 ms).
+//   param1, param2, ... → Optional parameters to pass to the function.
+
+setTimeout(function(){
+    console.log('Hello Everynone');   //  Runs after 5 seconds
+},5000);
+
+//   Output
+//   Hello Everyone
+
+
+
