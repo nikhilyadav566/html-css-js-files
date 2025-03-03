1196ec09bbc5a598232784f8cc89ae6bbceac9c7
nikhilyadav566 <nikhilyad42004@gmail.com>
2025-03-03 21:48:15 +0530

    Initial commit

---
+++ JavaScript/Lec25-Asynchronous/03-api.js
@@ -0,0 +1,14 @@
+// API :->
+
+//      API (Application Programming Interface) in JavaScript acts as an interface that allows different software components to communicate with each other. 
+
+//      It defines a set of rules and methods that developers can use to interact with external services, browsers, or system functionalities without needing to understand their internal implementation.
+
+//  Think of an API like a waiter in a restaurant 🍽️.
+
+//  1.  You (the user) order food (request).
+//  2.  The waiter (API) takes your order to the kitchen (server).
+//  3.  The kitchen prepares the food (processes the request).
+//  4.  The waiter brings the food back to you (response).
+
+//    In JavaScript, an API helps your code talk to other services, like fetching data from a website, getting weather updates, or saving information in the browser.
